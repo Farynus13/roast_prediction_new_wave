2cdec74dea1adb536eda024437454d1c05e4eb9b
Oskar Forreiter <forreitero@gmail.com>
2025-01-23 16:18:12 +0100

    new data preparation

--- data_preparation.py
+++ data_preparation.py
@@ -1,12 +1,10 @@
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 import os
-import tensorflow as tf
-import json 
-import ast
+import re
 
-def get_curves(dir_name,pre_roast_length=0):
+
+def get_curves(dir_name):
     
     file_names = os.listdir(dir_name)
     roasts = []
@@ -14,10 +12,10 @@ def get_curves(dir_name,pre_roast_length=0):
     for f_name in file_names:
         # print(f_name) 
         
-        success,bt_curve = extract_curve(f_name,dir_name,"'temp2':",pre_roast_length)
+        success,bt_curve = extract_curve(f_name,dir_name,"'temp2':")
         if success: # if we don't have enough pre roast data we don't use this roast
-            _,et_curve = extract_curve(f_name,dir_name,"'temp1':",pre_roast_length)
-            burner_curve = extract_burner(f_name,dir_name,pre_roast_length)
+            _,et_curve = extract_curve(f_name,dir_name,"'temp1':")
+            burner_curve = extract_burner(f_name,dir_name)
             np_bt_curve = np.array(bt_curve)
             np_et_curve = np.array(et_curve)
             np_burner_curve = np.array(burner_curve)
@@ -25,93 +23,35 @@ def get_curves(dir_name,pre_roast_length=0):
                 and len(np_bt_curve) == len(np_et_curve) == len(np_burner_curve)):
                 roast = np.column_stack((np_bt_curve,np_et_curve,np_burner_curve))
                 roasts.append(roast)
-                meta_data = create_meta_data(f_name)
+                meta_data = create_meta_data(dir_name+"/"+f_name)
                 meta_datas.append(meta_data)
-    meta_datas = one_hot_encoding(meta_datas,roasts,pre_roast_length)
     return roasts,meta_datas
 
-def one_hot_encoding(meta_datas,roasts,pre_roast_length=0):
-    origin = []
-    processing = []
-    for meta_data in meta_datas:
-        origin.append(meta_data['origin'])
-        processing.append(meta_data['processing'])
-    origin = pd.get_dummies(origin)
-    processing = pd.get_dummies(processing)
-    one_hot_encoded = pd.concat([origin,processing],axis=1)
-    one_hot_encoded = np.array(one_hot_encoded,dtype=bool)
 
-    roast_degrees = []
-    for roast in roasts:
-        if len(roast) > 600+pre_roast_length:
-            roast_degrees.append(1)
-        else:
-            roast_degrees.append(0)
-
-    roast_degrees = np.array(roast_degrees,dtype=bool)
-    one_hot_encoded = np.column_stack((one_hot_encoded,roast_degrees))
-    print(one_hot_encoded[:10])
-
-    return one_hot_encoded
 def create_meta_data(f_name):
-    meta_data = {}
-    # origin
-    meta_data['origin'] = get_origin(f_name)
-    meta_data['processing'] = get_processing(f_name)
+    """
+    Extracts moisture and density values from the file content.
+    
+    Args:
+        f_name (str): The file name containing the data.
+    
+    Returns:
+        np.ndarray: A NumPy array containing [moisture, density].
+    """
+    with open(f_name, 'r') as file:
+        content = file.read()
 
-    return meta_data
+    # Extract 'moisture_greens' value
+    moisture_match = re.search(r"'moisture_greens':\s*([\d.]+)", content)
+    moisture = float(moisture_match.group(1)) if moisture_match else None
 
-def get_processing(f_name):
-    washed_tags = ['washed','myta','kenia','la soledad','san jose','la maravilla','indie','uganda']
-    honey_tags = ['honey','costa','tajlandia']
-    natural_tags = ['natural','monjolo','fronteira','rwanda','nicaragua']
-    tags = {
-        'washed':washed_tags,
-        'honey':honey_tags,
-        'natural':natural_tags
-    }
-    for tag in tags:
-        for t in tags[tag]:
-            if t.lower() in f_name.lower():
-                return tag.lower()
-            
-    return 'other'
+    # Extract 'density' value
+    density_match = re.search(r"'density':\s*\[([\d.]+),\s*'g',\s*[\d.]+,\s*'l'\]", content)
+    density = float(density_match.group(1)) if density_match else None
 
-def get_origin(f_name):
-    brazil_tags = ['Brazylia','Brazil','Monjolo','Fazenda','California','Fronteira']
-    gwatemala_tags = ['Gwatemala','Guatemala','San Jose Del Lago','La Maravilla','La soledad']
-    colombia_tags = ['colombia','kolumbia','Jesus','ceron']
-    costarica_tags = ['Costa','Rica','Costa Rica','las lajas']
-    tajlandia_tags = ['Tajlandia','Thailand','Thajlandia','chiang','mai']
-    other_tags = ['uganda','honduras','indie','Myanmar','Nicaragua','Kenya','rwanda']
-    tags = {
-        'brazil':brazil_tags,
-        'gwatemala':gwatemala_tags,
-        'colombia':colombia_tags,
-        'costarica':costarica_tags,
-        'tajlandia':tajlandia_tags,
-    }
-    for tag in tags:
-        for t in tags[tag]:
-            if t.lower() in f_name.lower():
-                return tag.lower()
-    for t in other_tags:
-        if t.lower() in f_name.lower():
-            return t.lower()
-        
-    return 'other'
+    return np.array([moisture, density])
 
-            
-def extract_meta_data(f_name,dir_name):
-    # load whole data in the file into a single string
-    with open(dir_name+"/"+f_name,"r") as f:
-        lines = f.readlines()
-        data = "".join(lines)
-        data = ast.literal_eval(data)
-        print(data)
-
-
-def extract_burner(f_name,dir_name,pre_roast_length=0):
+def extract_burner(f_name,dir_name):
     events = []
     events_types = []
     events_value = []
@@ -128,7 +68,7 @@ def extract_burner(f_name,dir_name,pre_roast_length=0):
         
         charge_idx = get_charge(lines)
         drop_index = int(float(lines[lines.index("'DROP_time':")+1][:-1]))+charge_idx
-        start_idx = charge_idx-pre_roast_length
+        start_idx = charge_idx
         events_idx = lines.index("'specialevents':")
         event_types_idx = lines.index("'specialeventstype':")
         events_value_idx = lines.index("'specialeventsvalue':")
@@ -184,7 +124,7 @@ def extract_burner(f_name,dir_name,pre_roast_length=0):
             
         
             
-def extract_curve(f_name,dir_name,curve_name,pre_roast_length=0):
+def extract_curve(f_name,dir_name,curve_name):
     curve = []
     with open(dir_name+"/"+f_name,"r") as f:
             lines = [
@@ -196,8 +136,6 @@ def extract_curve(f_name,dir_name,curve_name,pre_roast_length=0):
             lines = lines[0]
 
             #calculate indexes of CHARGE and DROP with regard to reading points, to extract temp readings only during roast
-            if not has_enough_pre_roast_data(lines,pre_roast_length):
-                return False,curve
             charge_index = get_charge(lines)
             drop_index = int(float(lines[lines.index("'DROP_time':")+1][:-1]))+charge_index
             
@@ -206,7 +144,7 @@ def extract_curve(f_name,dir_name,curve_name,pre_roast_length=0):
             charge_index += curve_index+1
             drop_index += curve_index+1
             
-            current_index = charge_index-pre_roast_length
+            current_index = charge_index
             while current_index <= drop_index:
                 curve.append(float(lines[current_index][:-1]))
                 current_index += 1
@@ -229,24 +167,12 @@ def get_charge(lines):
     CHARGE_idx = TP_idx - TP_time
     
     return CHARGE_idx
-def has_enough_pre_roast_data(lines,pre_roast_length):
-    tp_info_index = lines.index("'TP_idx':")
-    TP_idx = lines[tp_info_index+1]
-    TP_time = lines[tp_info_index+3]
-    
-    #get rid of coma and cast to int
-    TP_idx = int(float(TP_idx[:-1]))
-    TP_time = int(float(TP_time[:-1]))
-
-    available_length = TP_idx - TP_time
-    return available_length >= pre_roast_length
 
 if __name__ == "__main__":
-    d_name = "data"
+    d_name = "roasts/annotated"
 
-    pre_roast_length = 75
-    data,meta_data = get_curves(d_name,pre_roast_length)
-
+    data,meta_data = get_curves(d_name)
+    print(meta_data[:5])
     data = data[:int(len(data)*0.9)]
     for roast in data:
             plt.plot(roast)
@@ -256,7 +182,7 @@ if __name__ == "__main__":
     clean_data = []
     clean_meta_data = []
     for roast,meta in zip(data,meta_data):
-        if roast.shape[0] > 400 and roast.shape[0] < 800:
+        if roast.shape[0] > 400 and roast.shape[0] < 800 and not meta[0] is None and not meta[1] is None:
             clean_data.append(roast)
             clean_meta_data.append(meta)
 
